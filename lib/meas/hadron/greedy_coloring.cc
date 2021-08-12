utku-k-can/chroma

commit 11cd31725128c69a8a2fb7ec4bc32c2840b20b2a

Generating new coloring schemes for disco.

lib/meas/hadron/greedy_coloring.cc
@@ -33,16 +33,38 @@ namespace Chroma
     // Vector of coordinates
     typedef std::vector<CoorType> Coors;
 
+    // Return the permutation [0,1,...]
+
+    CoorType naturalOrder()
+    {
+      CoorType c;
+      for (unsigned int i = 0; i < c.size(); i++)
+	c[i] = i;
+      return c;
+    }
+
+    // Return the permuation [N-1,N-2,...]
+
+    CoorType antiNaturalOrder()
+    {
+      CoorType c;
+      for (unsigned int i = 0; i < c.size(); i++)
+	c[i] = c.size() - i - 1;
+      return c;
+    }
+
     // Return the indices associated to each coordinate
     // \param coors: input coordinates
     // \param dim: lattice dimension
+    // \param order: ordering of the dimensions, anti-natural ordering by default
     //
     // Return a vector with indices of the passed coordinates in
     // the same order.
     //
     // NOTE: we used anti-natural order, the last coordinate moves the fastest
 
-    Indices coor2index(const Coors& coors, const CoorType dim)
+    Indices coor2index(const Coors& coors, const CoorType dim,
+		       const CoorType order = antiNaturalOrder())
     {
       // Quick exit
       if (dim.size() <= 0)
@@ -51,14 +73,17 @@ namespace Chroma
       // Output array
       Indices indices(coors.size());
 
-      // p(i) = prod(dim(end:-1:i))
+      // p(order(i)) = prod(dim(order(1:i)))
       CoorType p;
-      p.back() = 1;
-      for (int i = p.size() - 1; i >= 1; i--)
-	p[i - 1] = p[i] * dim[i];
+      p[order[0]] = 1;
+      for (unsigned int i = 1; i < dim.size(); i++)
+	p[order[i]] = p[order[i - 1]] * dim[order[i - 1]];
 
-      // indices(i) = inner product of coor(i) and p
-      // NOTE: every coordinate value is normalize to modulus the lattice dimension
+// indices(i) = inner product of coor(i) and p
+// NOTE: every coordinate value is normalize to modulus the lattice dimension
+#ifdef _OPENMP
+#  pragma omp parallel for schedule(static)
+#endif
       for (unsigned int i = 0; i < coors.size(); i++)
       {
 	IndexType r = 0;
@@ -73,13 +98,15 @@ namespace Chroma
     // Return the coordinates associated to each index
     // \param indices: input vertex indices
     // \param dim: lattice dimension
+    // \param order: ordering of the dimensions, anti-natural ordering by default
     //
     // Return a vector with the coordinates of the passed indices in
     // the same order.
     //
     // NOTE: we used anti-natural order, the last coordinate moves the fastest
 
-    Coors index2coor(const Indices& indices, const CoorType dim)
+    Coors index2coor(const Indices& indices, const CoorType dim,
+		     const CoorType order = antiNaturalOrder())
     {
       // Quick exit
       if (dim.size() <= 0)
@@ -93,37 +120,6 @@ namespace Chroma
       p.back() = 1;
       for (int i = p.size() - 1; i >= 1; i--)
 	p[i - 1] = p[i] * dim[i];
-
-      // coors(i,j) = indices(i) / p(i)
-      // NOTE: every coordinate value is normalize to modulus the lattice dimension
-      for (unsigned int i = 0; i < indices.size(); i++)
-	for (unsigned int j = 0; j < dim.size(); j++)
-	  coors[i][j] = (indices[i] / p[j]) % dim[j];
-
-      return coors;
-    }
-
-    // Return the coordinates associated to each index in natural order
-    // \param indices: input vertex indices
-    // \param dim: lattice dimension
-    //
-    // Return a vector with the coordinates of the passed indices in
-    // the same order.
-
-    Coors natindex2coor(const Indices& indices, const CoorType dim)
-    {
-      // Quick exit
-      if (dim.size() <= 0)
-	return Coors();
-
-      // Output array
-      Coors coors(indices.size());
-
-      // p(i) = prod(dim(1:i-1))
-      CoorType p;
-      p[0] = 1;
-      for (int i = 1; i < dim.size(); i++)
-	p[i] = p[i - 1] * dim[i - 1];
 
       // coors(i,j) = indices(i) / p(i)
       // NOTE: every coordinate value is normalize to modulus the lattice dimension
@@ -212,37 +208,73 @@ namespace Chroma
     Coors plus(const CoorType& c, const Coors& coors, const CoorType& dim)
     {
       Coors r(coors.size());
+#ifdef _OPENMP
+#  pragma omp parallel for schedule(static)
+#endif
       for (unsigned int i = 0; i < coors.size(); ++i)
 	for (unsigned int j = 0; j < dim.size(); ++j)
 	  r[i][j] = (dim[j] + coors[i][j] + c[j]) % dim[j];
       return r;
     }
 
+    IndexType normalize_coor(IndexType coor, IndexType dim)
+    {
+      return (dim == 0 ? 0 : coor % dim);
+    }
+
+    IndexType euclidian_dist_squared(const CoorType& a, const CoorType& b, const CoorType& dim)
+    {
+      CoorType d;
+      for (unsigned int i = 0; i < d.size(); ++i)
+	d[i] = normalize_coor(dim[i] + a[i] - b[i], dim[i]);
+      for (unsigned int i = 0; i < d.size(); ++i)
+	d[i] = std::min(d[i], dim[i] - d[i]);
+      IndexType dist = 0;
+      for (unsigned int i = 0; i < d.size(); ++i)
+	dist += d[i] * d[i];
+      return dist;
+    }
+
     // Return all neighbors up to a given distance WITH THE SAME PARITY
-    // \param dist: distance
+    // \param dist: distance in the z-direction
+    // \param power: all neighbors up to this distance
     //
     // Return a vector of coordinate differences to all neighbors up to
-    // the given distance and the same parity as the neighbors at the
+    // the given distance (power) and the same parity as the neighbors at the
     // given distance. The returned vector is used as input to the
     // function 'plus' to get all neighbors to a given vertex.
     //
     // NOTE: no performance requirements for this function; the function
     //       'plus' is doing the heavy lifting.
 
-    Coors neighbors_upto_distance(unsigned int dist, const CoorType& dim)
+    Coors neighbors_upto_distance(unsigned int dist, unsigned int power, const CoorType& dim)
     {
       // Find all neighbors of the vertex at origin up to the
       // given distance; a regular code should do something like:
       //   for i=1:dist, vertices=union(vertices, neighbors(vertices))
       // BUT get_motive ONLY CARES ABOUT EVEN VERTICES
-      Coors neighbors_pattern(1);
-      for (unsigned int i = 0; i < dist; i++)
+      Coors centers(2);
+      centers[0][2] = dist;
+      centers[1][2] = normalize_coor(dim[2] - dist, dim[2]);
+      Coors neighbors_pattern = centers, prev;
+      for (unsigned int i = 0; i < power; i++)
+      {
+	prev = neighbors_pattern;
 	neighbors_pattern = neighbors(neighbors_pattern, dim);
+	neighbors_pattern = index2coor(unique_and_sort(coor2index(neighbors_pattern, dim)), dim);
+      }
+      neighbors_pattern.insert(neighbors_pattern.end(), prev.begin(), prev.end());
+
+      // Filter our neighbors further than power distance in euclidian metric from the centers
+      Coors filter;
+      for (CoorType c : neighbors_pattern)
+	if (coor2index(Coors(1, c), dim)[0] != 0 &&
+	    (euclidian_dist_squared(c, centers[0], dim) <= power * power ||
+	     euclidian_dist_squared(c, centers[1], dim) <= power * power))
+	  filter.push_back(c);
 
       // Remove duplicate vertices
-      neighbors_pattern = index2coor(unique_and_sort(coor2index(neighbors_pattern, dim)), dim);
-
-      return neighbors_pattern;
+      return index2coor(unique_and_sort(coor2index(filter, dim)), dim);
     }
 
     // Return k-distance coloring
@@ -257,108 +289,69 @@ namespace Chroma
     // * First color even vertices with greedy coloring
     // * Then color odd vertices copying the coloring of the even vertices
 
-    Indices get_motive(unsigned int dist, const CoorType& dim, unsigned int& num_colors)
+    Indices get_colors(unsigned int dist, unsigned int power, const CoorType& dim,
+		       unsigned int& num_colors)
     {
-      dist = dist / 2 * 2;
-      Coors neighbors_rel = neighbors_upto_distance(dist, dim);
+      Coors neighbors_rel = neighbors_upto_distance(dist, power, dim);
 
       const unsigned int vol = volume(dim);
       std::vector<unsigned int> color(vol);
 
+      // For some reason, natural ordering works better for the tested lattices;
+      // also, it seems beneficial to swap the first and the last dimensions if
+      // the latter is the smallest
+      CoorType perm = naturalOrder();
+      if (dim[0] > dim[3])
+	std::swap(perm[0], perm[3]);
+
       // First color even vertices with greedy coloring
       num_colors = 0;
-      for (unsigned int i = 0; i < vol; i++)
+      for (int oddity = 0; oddity < 2; oddity++)
       {
-	// Get coordinates of vertex with index i
-	CoorType c = index2coor(Indices(1, i), dim)[0];
+	for (unsigned int i = 0; i < vol; i++)
+	{
+	  // Get coordinates of vertex with index i
+	  CoorType c = index2coor(Indices(1, i), dim, perm)[0];
 
-	// Only process even vertices: mod(sum(c),2) == 0
-	if (std::accumulate(c.begin(), c.end(), 0) % 2 != 0)
-	  continue;
+	  // Only process even vertices: mod(sum(c),2) == 0
+	  if (std::accumulate(c.begin(), c.end(), 0) % 2 != oddity)
+	    continue;
 
-	// Get neighbors of c
-	Indices c_neighbors = coor2index(plus(c, neighbors_rel, dim), dim);
+	  // Get neighbors of c
+	  Indices c_neighbors = coor2index(plus(c, neighbors_rel, dim), dim);
 
-	// Get the colors of the c's neighbors
-	std::vector<bool> used_colors(num_colors + 1);
-	for (unsigned int j = 0; j < c_neighbors.size(); ++j)
-	  used_colors[color[c_neighbors[j]]] = true;
+	  // Get the colors of the c's neighbors
+	  std::vector<bool> used_colors(num_colors + 1);
+	  for (unsigned int j = 0; j < c_neighbors.size(); ++j)
+	    used_colors[color[c_neighbors[j]]] = true;
 
-	// Find the first free color
-	// NOTE: color 0 means no color assigned
-	unsigned int j;
-	for (j = 1; j <= num_colors && used_colors[j]; j++)
-	  ;
+	  // Find the first free color
+	  // NOTE: color 0 means no color assigned
+	  unsigned int j;
+	  for (j = 1; j <= num_colors && used_colors[j]; j++)
+	    ;
 
-	// Assign the free color
-	color[i] = j;
+	  // Assign the free color
+	  unsigned int idx = coor2index(Coors(1, c), dim)[0];
+	  color[idx] = j;
 
-	// Track number of used colors
-	num_colors = std::max(num_colors, color[i]);
+	  // Track number of used colors
+	  num_colors = std::max(num_colors, color[idx]);
+	}
       }
-
-      // Color odd vertices as the color of the forward neighbor on
-      // the first direction
-      for (unsigned int i = 0; i < vol; i++)
-      {
-	// Get coordinates of vertex i
-	CoorType c = index2coor(Indices(1, i), dim)[0];
-
-	// Only process odd vertices
-	if (std::accumulate(c.begin(), c.end(), 0) % 2 != 1)
-	  continue;
-
-	// Get the color of the forward neighbor at the first direction
-	c[0]++;
-	color[i] = color[coor2index(Coors(1, c), dim)[0]] + num_colors;
-      }
-      num_colors *= 2;
 
       // Make zero the first color index
       for (unsigned int i = 0; i < vol; i++)
 	color[i]--;
 
-      //assert(check_coloring(color, dist, dim));
+      //assert(check_coloring(color, dist, power, dim));
       return color;
     }
 
-    // Return k-distance coloring
-    // \param dist: distance, k
-    // \param dim: lattice dimensions
-    //
-    // Return a vector with color value for every vertex following
-    // the anti-natural ordering.
-    //
-    // Strategy:
-    // * First color a tile of dimension 2^dist
-    // * Then color the lattice following the tile
-
-    Indices get_colors(unsigned int dist, const CoorType& dim, unsigned int& num_colors)
+    bool check_coloring(const Indices& color, unsigned int dist, unsigned int power,
+			const CoorType& dim)
     {
-      // The tile has dimension 2^dist if 2^dist is divisible by the original lattice
-      // dimension; otherwise the tile is as large as the lattice dimension
-      CoorType dim_motive;
-      unsigned int d = (1 << dist);
-      for (unsigned int i = 0; i < dim.size(); i++)
-	dim_motive[i] = (dim[i] % d == 0 ? d : dim[i]);
-
-      // dist-coloring the tile
-      Indices color_motive = get_motive(dist, dim_motive, num_colors);
-
-      // Color the original lattice following the coloring of the tile
-      const unsigned int vol = volume(dim);
-      std::vector<unsigned int> color(vol);
-      for (unsigned int i = 0; i < vol; i++)
-      {
-	color[i] = color_motive[coor2index(index2coor(Indices(1, i), dim), dim_motive)[0]];
-      }
-
-      return color;
-    }
-
-    bool check_coloring(const Indices& color, unsigned int dist, const CoorType& dim)
-    {
-      Coors neighbors_rel = neighbors_upto_distance(dist, dim);
+      Coors neighbors_rel = neighbors_upto_distance(dist, power, dim);
 
       const unsigned int vol = volume(dim);
       for (unsigned int i = 0; i < vol; i++)
@@ -380,15 +373,25 @@ namespace Chroma
   }
 
   // Construct a k-distance coloring
-  Coloring::Coloring(unsigned int distance)
+  Coloring::Coloring(unsigned int distance, unsigned int power)
   {
     // Get lattice dimensions
     CoorType latt_size;
     for (unsigned int i = 0; i < latt_size.size(); i++)
       latt_size[i] = Layout::lattSize()[i];
 
+    // Compute the tile size; the tile size should be divisible by the lattice size and
+    // greater or equal than 2*(dist+power)
+    CoorType tile_size;
+    for (unsigned int i = 0; i < latt_size.size(); i++)
+    {
+      tile_size[i] = std::min(2 * ((i == 2 ? distance : 0) + power), latt_size[i]);
+      while (latt_size[i] % tile_size[i] != 0)
+	tile_size[i]++;
+    }
+
     // Get colors for all nodes
-    Indices colors = get_colors(distance, latt_size, num_colors);
+    Indices colors = get_colors(distance, power, tile_size, num_colors);
 
     // Store the colors of the local nodes
     int this_node = Layout::nodeNumber();
@@ -402,7 +405,7 @@ namespace Chroma
       for (unsigned int j = 0; j < c.size(); j++)
 	c[j] = x[j];
 
-      local_colors[i] = colors[coor2index(Coors(1, c), latt_size)[0]];
+      local_colors[i] = colors[coor2index(Coors(1, c), tile_size)[0]];
     }
   }
 
@@ -428,7 +431,8 @@ namespace Chroma
 	{
 	  throw std::runtime_error("The coloring file has too many rows!");
 	}
-	colors[coor2index(natindex2coor(Indices(1, i), latt_size), latt_size)[0]] = idx - 1;
+	colors[coor2index(index2coor(Indices(1, i), latt_size), latt_size, naturalOrder())[0]] =
+	  idx - 1;
       }
       if (i < vol)
       {

lib/meas/hadron/greedy_coloring.h
@@ -16,7 +16,7 @@ namespace Chroma
   // Interface for computes distance-k coloring for toroidal lattices
   struct Coloring {
     // Construct a k-distance coloring
-    Coloring(unsigned int distance);
+    Coloring(unsigned int distance, unsigned int power);
     // Reading the coloring from a file
     Coloring(const std::string& filename);
 

lib/meas/inline/hadron/inline_disco_prob_defl_superb_w.cc
@@ -114,6 +114,12 @@ namespace Chroma
       else
 	param.probing_distance = param.max_path_length;
 
+      if(inputtop.count("probing_power")!=0){ 
+	read(inputtop,"probing_power",param.probing_power) ;
+      }
+      else
+	param.probing_power = 2;
+
       if(inputtop.count("probing_file")!=0){ 
 	read(inputtop,"probing_file",param.probing_file) ;
       }
@@ -143,6 +149,7 @@ namespace Chroma
       write(xml,"Propagator",param.prop) ;
       write(xml,"use_ferm_state_links",param.use_ferm_state_links) ;
       write(xml,"probing_distance",param.probing_distance) ;
+      write(xml,"probing_power",param.probing_power) ;
       write(xml,"noise_vectors",param.noise_vectors) ;
       write(xml,"max_rhs",param.max_rhs) ;
       xml << param.projParam.xml;
@@ -656,8 +663,10 @@ namespace Chroma
         QDPIO::cout << "Reading colors from file " << params.param.probing_file << std::endl;
         coloring.reset(new Coloring(params.param.probing_file));
       } else {
-        QDPIO::cout << "Generating a " << params.param.probing_distance << "-distance coloring" << std::endl;
-        coloring.reset(new Coloring(params.param.probing_distance));
+	QDPIO::cout << "Generating a " << params.param.probing_distance
+		    << "-distance coloring with a power " << params.param.probing_power
+		    << std::endl;
+	coloring.reset(new Coloring(params.param.probing_distance, params.param.probing_power));
       }
     
       //

lib/meas/inline/hadron/inline_disco_prob_defl_superb_w.h
@@ -44,6 +44,7 @@ namespace Chroma
 	ChromaProp_t prop;
         GroupXML_t projParam;
 	int probing_distance;
+	int probing_power;
         std::string probing_file;
 	int noise_vectors;
 	bool use_ferm_state_links ;

lib/meas/inline/hadron/inline_disco_prob_defl_w.cc
@@ -113,6 +113,12 @@ namespace Chroma
       else
 	param.probing_distance = param.max_path_length;
 
+      if(inputtop.count("probing_power")!=0){ 
+	read(inputtop,"probing_power",param.probing_power) ;
+      }
+      else
+	param.probing_power = 2;
+
       if(inputtop.count("probing_file")!=0){ 
 	read(inputtop,"probing_file",param.probing_file) ;
       }
@@ -142,6 +148,7 @@ namespace Chroma
       write(xml,"Propagator",param.prop) ;
       write(xml,"use_ferm_state_links",param.use_ferm_state_links) ;
       write(xml,"probing_distance",param.probing_distance) ;
+      write(xml,"probing_power",param.probing_power) ;
       write(xml,"noise_vectors",param.noise_vectors) ;
       write(xml,"max_rhs",param.max_rhs) ;
       xml << param.projParam.xml;
@@ -624,8 +631,10 @@ namespace Chroma
         QDPIO::cout << "Reading colors from file " << params.param.probing_file << std::endl;
         coloring.reset(new Coloring(params.param.probing_file));
       } else {
-        QDPIO::cout << "Generating a " << params.param.probing_distance << "-distance coloring" << std::endl;
-        coloring.reset(new Coloring(params.param.probing_distance));
+	QDPIO::cout << "Generating a " << params.param.probing_distance
+		    << "-distance coloring with a power " << params.param.probing_power
+		    << std::endl;
+	coloring.reset(new Coloring(params.param.probing_distance, params.param.probing_power));
       }
     
       //

lib/meas/inline/hadron/inline_disco_prob_defl_w.h
@@ -44,6 +44,7 @@ namespace Chroma
 	ChromaProp_t prop;
         GroupXML_t projParam;
 	int probing_distance;
+	int probing_power;
         std::string probing_file;
 	int noise_vectors;
 	bool use_ferm_state_links ;

commit 7183839c2dae3018c701690e3ffc0fdf0c534d38

Fix bug reading coloring schemes introduced in commit 11cd3172512; fix compiling issue with icpc

lib/meas/hadron/greedy_coloring.cc
@@ -35,6 +35,10 @@ namespace Chroma
 
     // Return the permutation [0,1,...]
 
+// Avoid intel compiler explosion
+#ifdef __INTEL_COMPILER
+#  pragma intel optimization_level 0
+#endif
     CoorType naturalOrder()
     {
       CoorType c;
@@ -45,6 +49,10 @@ namespace Chroma
 
     // Return the permuation [N-1,N-2,...]
 
+// Avoid intel compiler explosion
+#ifdef __INTEL_COMPILER
+#  pragma intel optimization_level 0
+#endif
     CoorType antiNaturalOrder()
     {
       CoorType c;
@@ -431,7 +439,7 @@ namespace Chroma
 	{
 	  throw std::runtime_error("The coloring file has too many rows!");
 	}
-	colors[coor2index(index2coor(Indices(1, i), latt_size), latt_size, naturalOrder())[0]] =
+	colors[coor2index(index2coor(Indices(1, i), latt_size, naturalOrder()), latt_size)[0]] =
 	  idx - 1;
       }
       if (i < vol)